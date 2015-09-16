26067c460c92f080d82c292bc3d4fc3205aa4db1
Evan Schultz <evan@rangle.io>
2015-09-16 15:14:32 -0400

    Revert "expirment"
    This reverts commit de55912d2b0e8b1f2504839a7c846a631b4cd9d9.

--- src/reducers/table-reducer.js
+++ src/reducers/table-reducer.js
@@ -40,7 +40,7 @@ export default function tableReducer(state = INITIAL_STATE, action) {
   if (!action.type || !action.payload) {
     return state;
   }
-
+  
   let tableIndex = findIndex(state, action.payload.tableId);
   switch (action.type) {
   case PARTY_SEATED:
@@ -55,9 +55,8 @@ export default function tableReducer(state = INITIAL_STATE, action) {
     }
   case ITEM_ADDED:
     {
-      let result = state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value + 1);
-      console.log('the result',result.toJS());
-      return result;
+
+      return state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value + 1);
     }
   case ITEM_REMOVED:
     {
