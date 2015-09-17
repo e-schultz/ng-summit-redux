0792408fbc75a54e555c270b072ea1c1b60346d3
Evan Schultz <evan@rangle.io>
2015-09-16 21:39:44 -0400

    Remove unneeded onChange from pending-orders

--- src/components/pending-orders/pending-orders-controller.js
+++ src/components/pending-orders/pending-orders-controller.js
@@ -4,34 +4,11 @@ import {ORDERING} from '../../reducers/table-reducer';
 export default class PendingOrdersController {
   constructor($ngRedux, $scope) {
 
-    let _onChange = (state) => {
-      let pendingOrders = state.tables.filter(n => n.get('status') === ORDERING);
-      return {
-        orders: pendingOrders.map(n => {
-          return {
-            tableId: n.get('id'),
-            items: n.get('order').map((value, key) => {
-              let menuItem = state.menu.find(menuItem => menuItem.get('menuId') === key);
-              return {
-                menuId: key,
-                qty: value,
-                description: menuItem.get('description'),
-                price: menuItem.get('price'),
-                total: value * menuItem.get('price')
-              };
-
-            }).toArray()
-
-          };
-        })
-      };
-    };
-
-    let disconnect = $ngRedux.connect(_onChange)(this);
-
+    let disconnect = $ngRedux.connect(state => this.onUpdate(state))(this);
     $scope.$on('$destroy', () => disconnect());
   }
- mapOrders(order, menu) {
+
+  mapOrders(order, menu) {
     return {
       tableId: order.get('id'),
       items: order.get('order').map((value, key) => {
