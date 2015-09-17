97c52e83b08323d8d8fd641dc5faf02246d7f98e
Evan Schultz <evan@rangle.io>
2015-09-17 18:03:25 -0400

    change format of table reducer back

--- src/reducers/table-reducer.js
+++ src/reducers/table-reducer.js
@@ -7,7 +7,6 @@ from '../actions/table-actions.js';
 import {CLEAN, DIRTY, OCCUPIED, ORDERING, ORDERED, HAS_FOOD} from '../constants';
 /* beautify preserve:end */
 
-
 export const INITIAL_STATE = fromJS([{
     id: 1,
     numberOfSeats: 2,
@@ -33,67 +32,31 @@ export const INITIAL_STATE = fromJS([{
 }]);
 
 export default function tableReducer(state = INITIAL_STATE, action) {
-  const findIndex = (collection, id) => collection.findIndex(n => n.get('id') === id);
+  let findIndex = (collection, id) => collection.findIndex(n => n.get('id') === id);
 
   if (!action.type || !action.payload) {
     return state;
   }
-  const tableIndex = findIndex(state, action.payload.tableId);
-  const actions = {
-    [PARTY_SEATED]: (state) => state.setIn([tableIndex, 'status'], OCCUPIED),
-    [ORDER_STARTED]:  (state) => state.setIn([tableIndex, 'status'], ORDERING),
-    [ITEM_ADDED]: (state) => state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value + 1),
-    [ITEM_REMOVED]: (state) => state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value === 0 ? 0 : value - 1),
-    [CUSTOMER_PAID]: (state) => state.setIn([tableIndex, 'status'], DIRTY),
-    [TABLE_CLEANED]: (state) => state.setIn([tableIndex, 'status'], CLEAN),
-    [ORDER_COMPLETED]: (state) => state.setIn([tableIndex, 'status'], ORDERED),
-    [ORDER_DELIVERED]: (state) => state.setIn([tableIndex, 'status'], HAS_FOOD)
-  };
 
-  return actions[action.type] ? actions[action.type](state) : state;
-/*
   let tableIndex = findIndex(state, action.payload.tableId);
   switch (action.type) {
   case PARTY_SEATED:
-    {
-
-      return state.setIn([tableIndex, 'status'], OCCUPIED);
-    }
+    return state.setIn([tableIndex, 'status'], OCCUPIED);
   case ORDER_STARTED:
-    {
-
-      return state.setIn([tableIndex, 'status'], ORDERING);
-    }
+    return state.setIn([tableIndex, 'status'], ORDERING);
   case ITEM_ADDED:
-    {
-
-      return state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value + 1);
-    }
+    return state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value + 1);
   case ITEM_REMOVED:
-    {
-      
-      return state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value === 0 ? 0 : value - 1);
-    }
-
+    return state.updateIn([tableIndex, 'order', action.payload.menuItemId], 0, value => value === 0 ? 0 : value - 1);
   case 'CUSTOMER_PAID':
-    {
-
-      return state.setIn([tableIndex, 'status'], DIRTY);
-    }
+    return state.setIn([tableIndex, 'status'], DIRTY);
   case 'TABLE_CLEANED':
-    {
-
-      return state.setIn([tableIndex, 'status'], CLEAN);
-    }
+    return state.setIn([tableIndex, 'status'], CLEAN);
   case ORDER_COMPLETED:
-    {
-       return state.setIn([tableIndex, 'status'], ORDERED);
-    }
-    case ORDER_DELIVERED:
-    {
-     return state.setIn([tableIndex, 'status'], HAS_FOOD); 
-    }
+    return state.setIn([tableIndex, 'status'], ORDERED);
+  case ORDER_DELIVERED:
+    return state.setIn([tableIndex, 'status'], HAS_FOOD);
   default:
     return state;
-  }*/
+  }
 }
