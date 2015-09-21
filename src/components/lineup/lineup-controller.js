4c26d809a295c380ee0b5d3e1fa6f12b99af28d4
Evan Schultz <evan@rangle.io>
2015-09-21 17:39:37 -0400

    Formatting

--- src/components/lineup/lineup-controller.js
+++ src/components/lineup/lineup-controller.js
@@ -3,8 +3,12 @@ import lineupActions from '../../actions/lineup-actions';
 export default class LineupController {
   constructor($ngRedux, $scope) {
 
-    let disconnect = $ngRedux.connect(state => this.onUpdate(state), lineupActions)(this);
-    $scope.$on('$destroy', () => disconnect());
+    let disconnect = $ngRedux.connect(
+        state => this.onUpdate(state),  // What we want to map to our target
+        lineupActions                   // Actions we want to map to our target
+        )(this);                        // Our target
+    
+    $scope.$on('$destroy', disconnect); // Cleaning house
   }
 
   onUpdate(state) {
