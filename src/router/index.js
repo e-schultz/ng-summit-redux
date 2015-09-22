2c90b5730a843087f30af259c837d25ab340b1df
Evan Schultz <evan@rangle.io>
2015-09-22 19:45:24 -0400

    remove router-v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
-import routerConfig from './router-config-v1';
+import routerConfig from './router-config';
 
 export default angular
   .module('app.router', [
