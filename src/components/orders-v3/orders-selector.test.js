c3efebc4412f56e881c4f9f42e705e939f0995a7
Evan Schultz <evan@rangle.io>
2015-09-28 12:59:26 -0400

    clean up orders-selector test

--- src/components/orders-v3/orders-selector.test.js
+++ src/components/orders-v3/orders-selector.test.js
@@ -6,57 +6,9 @@ import {ORDERING, ORDERED} from '../../constants';
 
 /* beautify preserve:end */
 
-describe.only('the orders selector', () => {
-  /*describe('the menu selector', () => {
-    it('should return the menu from the state object', () => {
-      const state = {
-        menu: [{
-          menuId: 'test '
-        }]
-      };
-
-      const result = menuSelector(state);
-      expect(result).to.be.deep.equal(state.menu);
-    });
-  });
-
-  describe('pendingOrders', () => {
-    it('should only return items that have a pending order status', () => {
-      const state = {
-        tables: [{
-          status: ORDERING
-        }, {
-          status: ORDERED
-        }]
-      };
-
-      const result = pendingOrders(state);
-      expect(result.length).to.equal(1);
-      expect(result[0].status).to.equal(ORDERING);
-
-    });
-  });
-
-  describe('completedOrders', () => {
-    it('should only return items that have a pending order status', () => {
-      const state = {
-        tables: [{
-          status: ORDERING
-        }, {
-          status: ORDERED
-        }]
-      };
-
-      const result = completedOrders(state);
-      expect(result.length).to.equal(1);
-      expect(result[0].status).to.equal(ORDERED);
-
-    });
-  });*/
-
-  let mockState;
-  beforeEach(() => {
-    mockState = {
+describe('the orders selector', () => {
+  
+  const mockState = {
       menu: [{
         menuId: 'pancake', description: 'Stack of Pancakes',
         stock: 50, price: 1.99
@@ -70,7 +22,7 @@ describe.only('the orders selector', () => {
         order: {pancake: 2}
         }]
     };
-  });
+  
 
   it('should return a collection of pending and completed orders', () => {
     const result = ordersSelector(mockState);
@@ -81,19 +33,26 @@ describe.only('the orders selector', () => {
   });
 
   it('should convert order to a collection of items', () => {
+    
     const result = ordersSelector(mockState);
-    let pendingOrders = result.pending[0];
-    let completedOrders = result.completed[0];
+    
+    const pendingOrders = result.pending[0];
+    const completedOrders = result.completed[0];
+    
     expect(pendingOrders).to.have.property('items');
     expect(completedOrders).to.have.property('items');
 
   });
   it('should calculate the total of the items in the order', () => {
+  
     const result = ordersSelector(mockState);
-    let pendingItems = result.pending[0].items;
-    let completedItems = result.completed[0].items;
+    
+    const pendingItems = result.pending[0].items;
+    const completedItems = result.completed[0].items;
+    
     expect(pendingItems[0].total).to.be.equal(1.99);
     expect(completedItems[0].total).to.be.equal(1.99 * 2);
+
   });
 
 });
