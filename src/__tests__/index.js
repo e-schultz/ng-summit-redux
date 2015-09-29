9d1c2ef04272cc14523d1634578405ae89d41756
Evan Schultz <evan@rangle.io>
2015-09-29 17:50:40 -0400

    Revert "Chore initial project setup"

--- src/__tests__/index.js
+++
@@ -1,6 +0,0 @@
-
-import chai from 'chai';
-import chaiImmutable from 'chai-immutable';
-chai.use(chaiImmutable);
-global.localStorage = require('localStorage')
-global.expect = chai.expect;
--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const CLEAN = 'CLEAN';
-export const DIRTY = 'DIRTY';
-export const OCCUPIED = 'OCCUPIED';
-export const ORDERING = 'ORDERING';
-export const ORDERED = 'ORDERED';
-export const HAS_FOOD = 'HAS_FOOD';
